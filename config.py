f06c6f8395de62d8f334bafc58677ad912710d9d
shareef1232 <shareeftamadaddi70@gmail.com>
2025-04-12 20:08:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://mammoth-glennis-moviebot5-e399c723.koyeb.app/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
